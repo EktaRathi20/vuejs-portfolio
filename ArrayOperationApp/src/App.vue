82888601acd0499dec78dd18b6350b0d7a3a1496
EktaRathi20 <rathiekta20@gmail.com>
2024-03-10 14:37:23 +0530

    arrayOperationApp

---
+++ ArrayOperationApp/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<button @click="isShow=!isShow">Toggle</button>
+<button @click="list.push(list.length+1)">Push item</button>
+<button @click="list.pop(list.length-1)">Pop item</button>
+<button @click="list.reverse()">Reverse List</button>
+<ul v-if="isShow && list.length">
+  <li v-for="(item,index) of list" :key="index">
+    {{item}}
+  </li>
+</ul>
+<p v-else-if="list.length">List is not empty, but hidden.</p>
+<p v-else>List is empty.</p>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      list:[1,2,3],
+      isShow:true
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
