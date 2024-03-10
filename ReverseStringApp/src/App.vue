484b20ea3fa22e5f0f04fd42c59ebdcc73c99276
EktaRathi20 <rathiekta20@gmail.com>
2024-03-10 14:36:14 +0530

    reverseStringApp

---
+++ ReverseStringApp/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+{{message}}
+<br/>
+<button @click="reverse">Reverve</button>
+<br/>
+<button @click="message+='!'">Append=>"!"</button>
+<br/>
+<a href="www.google.com"  @click.prevent="notify">click me</a>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      message:"Hello World"
+    }
+  },
+  methods:{
+    reverse(){
+      this.message=this.message.split('').reverse().join('')
+    },
+    notify(){
+      alert('navigation prevented')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
