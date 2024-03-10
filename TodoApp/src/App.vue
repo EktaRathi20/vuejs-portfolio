4b1432f23576f4e7cff959df552e26514635a38d
EktaRathi20 <rathiekta20@gmail.com>
2024-03-10 14:33:47 +0530

    todoApp

---
+++ TodoApp/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+
+<addTodo/>
+<filterTodo/>
+<showTodo/>
+
+
+</template>
+
+<script>
+
+import addTodo from './components/addTodo.vue'
+import filterTodo from './components/filterTodo.vue'
+import showTodo from './components/showTodo.vue'
+export default {
+  
+  name: 'App',
+  components: {
+    addTodo,
+    filterTodo,
+    showTodo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
